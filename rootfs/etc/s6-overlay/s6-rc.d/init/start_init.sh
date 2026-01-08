8d01b773cd86f6fe271ea62f5152ad7f31c2583c
tuxsu <dzlzy@proton.me>
2026-01-08 09:22:45 +0800

    fix init user script

--- rootfs/etc/s6-overlay/s6-rc.d/init/start_init.sh
+++ rootfs/etc/s6-overlay/s6-rc.d/init/start_init.sh
@@ -11,40 +11,31 @@ if [ "$PUID" -eq 0 ] || [ "$PGID" -eq 0 ]; then
     exit 0
 fi
 
-if ! getent group deluge >/dev/null; then
-    EXISTING_GROUP=$(getent group "$PGID" | cut -d: -f1)
+if ! grep -q "^deluge:" /etc/group; then
+    EXISTING_GROUP=$(grep ":$PGID:" /etc/group | cut -d: -f1)
     if [ -n "$EXISTING_GROUP" ]; then
-        echo "Warning: GID $PGID is already used by group '$EXISTING_GROUP'. Renaming to 'deluge'..."
-        groupmod -n deluge "$EXISTING_GROUP"
+        echo "Warning: GID $PGID is used by '$EXISTING_GROUP'. Renaming..."
+        sed -i "s/^$EXISTING_GROUP:/deluge:/" /etc/group
     else
         echo "Creating group 'deluge' with GID $PGID..."
-        addgroup -g "$PGID" -S deluge
+        addgroup -g "$PGID" -S deluge 2>/dev/null || groupadd -g "$PGID" deluge
     fi
 else
-    CURRENT_GID=$(getent group deluge | cut -d: -f3)
-    if [ "$CURRENT_GID" != "$PGID" ]; then
-        echo "Updating GID for 'deluge' from $CURRENT_GID to $PGID..."
-        groupmod -g "$PGID" deluge
-    fi
+    sed -i "s/^deluge:x:[0-9]\+:/deluge:x:$PGID:/" /etc/group
 fi
 
-if ! id -u deluge >/dev/null 2>&1; then
-    EXISTING_USER=$(getent passwd "$PUID" | cut -d: -f1)
+if ! grep -q "^deluge:" /etc/passwd; then
+    EXISTING_USER=$(grep "x:$PUID:" /etc/passwd | cut -d: -f1)
     if [ -n "$EXISTING_USER" ]; then
-        echo "Warning: UID $PUID is already used by user '$EXISTING_USER'. Renaming to 'deluge'..."
-        usermod -l deluge "$EXISTING_USER"
-        usermod -g "$PGID" deluge
+        echo "Warning: UID $PUID is used by '$EXISTING_USER'. Renaming..."
+        sed -i "s/^$EXISTING_USER:/deluge:/" /etc/passwd
     else
         echo "Creating user 'deluge' with UID $PUID..."
-        adduser -u "$PUID" -G deluge -S -D -H deluge
+        adduser -u "$PUID" -G deluge -S -D -H deluge 2>/dev/null || \
+        useradd -u "$PUID" -g deluge -m -s /bin/sh deluge
     fi
 else
-    CURRENT_UID=$(id -u deluge)
-    CURRENT_USER_GID=$(id -g deluge)
-    if [ "$CURRENT_UID" != "$PUID" ] || [ "$CURRENT_USER_GID" != "$PGID" ]; then
-        echo "Updating 'deluge' user (UID: $CURRENT_UID->$PUID, GID: $CURRENT_USER_GID->$PGID)..."
-        usermod -u "$PUID" -g "$PGID" deluge
-    fi
+    sed -i "s/^deluge:x:[0-9]\+:[0-9]\+:/deluge:x:$PUID:$PGID:/" /etc/passwd
 fi
 
 echo "Applying file permissions..."
