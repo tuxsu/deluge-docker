477cabdb1dadecb1cef8830e81cf8c1549b7c72a
tuxsu <dzlzy@proton.me>
2026-01-09 10:32:35 +0800

    fix deluge config args

--- rootfs/etc/s6-overlay/s6-rc.d/deluge-web/start.sh
+++ rootfs/etc/s6-overlay/s6-rc.d/deluge-web/start.sh
@@ -1,7 +1,7 @@
 #!/command/with-contenv sh
-DELUGE_CONFIG_DIR=${DELUGE_CONFIG_DIR:-/config}
+CONFIG_DIR=${CONFIG_DIR:-/config}
 if [ "$PUID" -eq 0 ] || [ "$PGID" -eq 0 ]; then
-	exec deluge-web -c "$DELUGE_CONFIG_DIR" -L info 2>&1
+	exec deluge-web -c "$CONFIG_DIR" -L info 2>&1
 else
-	exec s6-setuidgid deluge deluge-web -c "$DELUGE_CONFIG_DIR" -L info 2>&1
+	exec s6-setuidgid deluge deluge-web -c "$CONFIG_DIR" -L info 2>&1
 fi
